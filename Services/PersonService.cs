e3d749bba1c58affc4733cba0909ec008281a140
Yi <soujirofb@gmail.com>
2024-05-22 20:21:15 +0800

    實作  PersonService AddPerson
    實作  PersonService AddPerson

--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -1,4 +1,5 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 using System;
 
@@ -6,9 +7,49 @@ namespace Services
 {
     public class PersonService : IPersonsService
     {
-        public PersonResponse AddPerson(PersonAddRequest personAddRequest)
+        //privatte field
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+
+        public PersonService()
         {
-            throw new NotImplementedException();
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+        private PersonResponse CovertPersonToPersonResponse(Person person)
+        {
+            PersonResponse personResponse = person.ToPersonResponse();
+            personResponse.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+
+            return personResponse;
+        }
+        public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+        {
+            // 檢查PersonAddRequest 是否為null
+            if (personAddRequest is null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            //驗證 PersonName
+            if (string.IsNullOrWhiteSpace(personAddRequest.PersonName))
+            {
+                throw new ArgumentException("Person Name can't be blank");
+            }
+
+            //convert personAddRequest into Person type
+            Person person = personAddRequest.ToPerson();
+
+            //generate PersonID
+            person.PersonID = Guid.NewGuid();
+
+            // add person object to persons list
+            _persons.Add(person);
+
+            //convert the Person object into PersonResponse type
+
+            return CovertPersonToPersonResponse(person);
         }
 
         public List<PersonResponse> GetAllPerson()
