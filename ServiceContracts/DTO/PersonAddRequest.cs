5e6e6f6c61ebdd074956c3a180015c0066fd6440
Yi <soujirofb@gmail.com>
2024-06-04 20:04:25 +0800

    Create Person 頁面引入JQuery 加入 client端驗證
    Create Person 頁面引入JQuery 加入 client端驗證

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -21,7 +21,11 @@ namespace ServiceContracts.DTO
 
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Please select a gender of the person")]
         public GenderOptions? Gender { get; set; }
+
+        [Required(ErrorMessage ="Please select a country")]
         public Guid? CountryID { get; set; }
         public string? Address { get; set; }
         public bool ReceiveNewsLetters { get; set; }
